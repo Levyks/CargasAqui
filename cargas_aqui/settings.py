66f5f2cc76f4ef5d7b5c8c119942c0ef8c7f279b
Levy Barbosa <marcio.levy20@gmail.com>
2022-05-04 21:10:31 -0300

    added translations

--- cargas_aqui/settings.py
+++ cargas_aqui/settings.py
@@ -118,7 +118,7 @@ AUTH_USER_MODEL = 'core.User'
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
