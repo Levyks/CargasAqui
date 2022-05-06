f0fa50d03f9841d8a3cc50f4840f610b379a06da
Levy Barbosa <marcio.levy20@gmail.com>
2022-05-06 16:25:14 -0300

    added auto reload to admin cargo listing

--- cargas_aqui/core/static/js/admin/cargo.js
+++ cargas_aqui/core/static/js/admin/cargo.js
@@ -1,5 +1,28 @@
 (function ($) {
   $(function() {
     $('#id_driverPhone').mask('(99) 99999-999?9');
+
+    if(!!$("#changelist").length) {
+    
+      var timeout;
+
+      function scheduleReload() {
+        console.log("agendando");
+        timeout = setTimeout(function() {
+          console.log("reloadando");
+          location.reload();
+        }, 60 * 1000);
+      }
+
+      scheduleReload();
+
+      $(window).on('click', function() {
+        console.log("limpando");
+        clearTimeout(timeout);
+        scheduleReload();
+      });
+    
+    }
   });
+
 })(django.jQuery);
